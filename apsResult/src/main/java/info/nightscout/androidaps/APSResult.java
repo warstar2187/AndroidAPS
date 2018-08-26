fdfd06eda9efca0e93cff2e553186e6a5eae38b2
Milos Kozak <m.kozak@sysop.cz>
2018-08-26 11:43:19 +0200

    APSResult module, 'gradlew fatJar' working

---
+++ apsResult/src/main/java/info/nightscout/androidaps/APSResult.java
@@ -0,0 +1,11 @@
+package info.nightscout.androidaps;
+
+public class APSResult {
+    public APSResult() {
+    }
+
+    public static void main(String[] args) {
+        System.out.print("APSResult start");
+
+    }
+}
